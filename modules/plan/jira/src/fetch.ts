21738958d2b1f8fcb3c0f7b7ee0f0efa6b37b951
Grzegorz Abramczyk <abramczykg@gmail.com>
2021-01-01 21:36:58 +0100

    Fetch service implementation

---
+++ modules/plan/jira/src/fetch.ts
@@ -0,0 +1,32 @@
+import { Issue } from './model'
+import axios, {AxiosInstance } from 'axios'
+
+const batchSize = process.env.JIRA_BATCH_SIZE ? Number.parseInt(process.env.JIRA_BATCH_SIZE) : 500
+export class Fetch {
+  constructor(private client: AxiosInstance) {}
+
+  public async* jql(jql: string): AsyncGenerator<Issue> {
+    let startAt = 0
+    while(true) {
+      const response = await this.client.post('/rest/api/2/search', {
+        jql, fields: ['*all'], maxResults: batchSize, startAt
+      })
+      if (response.data.issues && response.data.issues.length) {
+        yield* response.data.issues
+        startAt += batchSize
+      } else {
+        break
+      }
+    }
+
+  }
+}
+
+const fetch = new Fetch(axios.create({
+  baseURL:'http://jira.home.agrzes.pl:8080',
+  auth: {
+    username: process.env.JIRA_USERNAME,
+    password: process.env.JIRA_PASSWORD
+  }
+}))
+export default fetch
