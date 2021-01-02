f6bdd5d8afff9e703afdabb0ea489cce13f5d7b0
Grzegorz Abramczyk <abramczykg@gmail.com>
2021-01-02 20:48:16 +0100

    Further tests

--- modules/plan/jira/test/jira/schema.ts
+++ modules/plan/jira/test/jira/schema.ts
@@ -1,5 +1,11 @@
-import {mapFieldDefinitionToJSONSchema} from '../../src/jira/schema'
-import {expect} from 'chai'
+import {mapFieldDefinitionToJSONSchema, Schema} from '../../src/jira/schema'
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import { AxiosInstance } from 'axios'
+const {expect} = chai.use(chaiAsPromised).use(sinonChai)
+
 describe('jira', function() {
   describe('schema', function() {
     describe('mapFieldDefinitionToJSONSchema', function() {
@@ -68,6 +74,37 @@ describe('jira', function() {
           id: 'thumbnail'
         })
         expect(mapped).to.be.false
+      })
+    })
+    describe('Schema', function() {
+      const common = {
+        name: 'name',
+        clauseNames: [],
+        custom: false,
+        id: 'id',
+        navigable: true,
+        orderable: true,
+        searchable: true,
+      }
+      it('should retrieve fields',async function() {
+        const client = {
+          get: sinon.stub().resolves({data:[]})
+        } as unknown as AxiosInstance
+        const map = sinon.stub()
+        const schema = new Schema(client,map)
+        await schema.fields()
+        expect(client.get).to.be.calledWith('/rest/api/2/field')
+      })
+      it('should map fields',async function() {
+        const client = {
+          get: sinon.stub().resolves({data:[{id:'a'}]})
+        } as unknown as AxiosInstance
+        const map = sinon.stub().returns('b')
+        const schema = new Schema(client,map)
+        const result = await schema.fields()
+        expect(map).to.be.calledWith({id:'a'})
+        expect(result).to.have.property('type','object')
+        expect(result).to.have.deep.property('properties',{a:'b'})
       })
     })
   })
